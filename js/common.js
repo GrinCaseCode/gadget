88e1bf4be696dd3468d109ec3dd6909aa61737f5
nickolay.grin <nick@singularitysolutions.com>
2023-12-15 11:43:08 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -191,6 +191,38 @@ if ( $(this).scrollTop() > 0 && $menu.hasClass("default") ){
 
 	$(".input-phone").mask("+7 (999) 999-99-99");
 
+	jQuery('.quantity').each(function() {
+		var spinner = jQuery(this),
+		input = spinner.find('input[type="number"]'),
+		btnUp = spinner.find('.quantity-up'),
+		btnDown = spinner.find('.quantity-down'),
+		min = input.attr('min'),
+		max = input.attr('max');
+
+		btnUp.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+	});
+
 
 	 // стайлер для select
 	 $('select').styler();
